1f712db084a1f9dff7e9a56f1e964d29866d5c9a
AmanullahLalwani <amanullahamjad342@gmail.com>
2024-06-29 21:27:37 +0500

    simplewhatherapp

---
+++ script .js
@@ -0,0 +1,15 @@
+let userInput = prompt("What Weather In Your Area?");
+
+if(userInput =>! "50"){
+    alert("Stay Home")
+
+
+    
+}else if(userInput => "40"){
+    alert("Use Umberalla")
+
+}else if(userInput => "30"){
+    alert("Its a Nice Day")
+}else{
+
+}
